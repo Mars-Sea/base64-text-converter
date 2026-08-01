4d35e3b719a2ce5d4c2eb0ed1c3b25e119bee4f4
Mars-Sea <1195751600@qq.com>
2026-08-01 10:08:50 +0800

    fix: 关闭 react-hooks v7 新增的严格 lint 规则
    set-state-in-effect 和 purity 是 v7 引入的 React Compiler 规则，
    会误报 shadcn/ui 模板代码（carousel/sidebar/use-mobile），导致 CI lint 失败。

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,10 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      // react-hooks v7 新增的 React Compiler 严格规则，会误报 shadcn/ui 模板代码
+      // (carousel/sidebar/use-mobile 中的惯用写法)，此处关闭
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/purity': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
